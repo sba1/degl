672b11b9742df2b958f5f02d4fb8d6ad2161b6b9
Sebastian Bauer <mail@sebastianbauer.info>
2014-11-10 17:25:47 +0100

    Added new preinitialized data and accessors.

--- simple/simplelib.c
+++ simple/simplelib.c
@@ -1,4 +1,5 @@
 static int global;
+static short sh_global = 2;
 
 void set_global(int value)
 {
@@ -11,3 +12,12 @@ int get_global(void)
 	return global;
 }
 
+void set_sh_global(short value)
+{
+	sh_global = value;
+}
+
+short get_sh_global(void)
+{
+	return sh_global;
+}
