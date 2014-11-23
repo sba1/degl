7b3cfdbe992e66c05693fb42a160f138a4f4b656
Sebastian Bauer <mail@sebastianbauer.info>
2014-11-23 22:46:44 +0100

    Introduced second set of files that we try to transform.
    Obviously, this doesn't work yet.

--- core_test.cpp
+++ core_test.cpp
@@ -1,6 +1,10 @@
 #include "core.cpp"
 
-int main(int argc, char **argv) {
-	transform("simplelib/func1.c");
+int main(int argc, char **argv)
+{
+	std::vector<const char *> files;
+	files.push_back("simplelib/func1.c");
+	files.push_back("simplelib/func2.c");
+	transform(files);
 	return 0;
 }
---
+++ simplelib/func2.c
@@ -0,0 +1,8 @@
+#include "func2.h"
+
+#include "func1.h"
+
+void get_global_2(void)
+{
+	return get_global();
+}
